2aaccc9b6b26a858e68580a61616b23b521641cf
marwo22 <1mkwozniak@gmail.com>
2020-09-26 14:18:13 +0200

    started entities

--- src/main/java/com/sda/geoproject/external/borehole/DatabaseBoreholeRepository.java
+++ src/main/java/com/sda/geoproject/external/borehole/DatabaseBoreholeRepository.java
@@ -1,4 +1,40 @@
 package com.sda.geoproject.external.borehole;
 
-public class DatabaseBoreholeRepository {
+import com.sda.geoproject.domain.borehole.Borehole;
+import com.sda.geoproject.domain.borehole.BoreholeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseBoreholeRepository implements BoreholeRepository {
+    private JpaBoreholeRepository boreholeRepository;
+
+    @Override
+    public void createB(Borehole borehole) {
+
+    }
+
+    @Override
+    public void deleteB(int id) {
+
+    }
+
+    @Override
+    public void updateB(Borehole borehole) {
+
+    }
+
+    @Override
+    public Optional<Borehole> getOne(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Borehole> getAll() {
+        return null;
+    }
 }
