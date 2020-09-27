30a27d7572fd978cc4efaffbf6e86c11265ce58b
marwo22 <1mkwozniak@gmail.com>
2020-09-27 09:55:40 +0200

    probe entity

--- src/main/java/com/sda/geoproject/external/probe/JpaProbeRepository.java
+++ src/main/java/com/sda/geoproject/external/probe/JpaProbeRepository.java
@@ -1,8 +1,12 @@
 package com.sda.geoproject.external.probe;
 
+import com.sda.geoproject.external.borehole.BoreholeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JpaProbeRepository extends JpaRepository<ProbeEntity, Integer> {
+    Optional<ProbeEntity> findByLocation(String location);
 }
